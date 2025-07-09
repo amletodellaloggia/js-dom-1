469bad71f4f650d656558862af826e1d4a091ea9
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-09 15:47:00 +0200

    [upd] script (button change, event listener 2 removed, else  if comments), img crop and replacement for visual aid

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ const img = document.getElementById(`lampOff`); // Dichiaro una variabile che ac
 
 button.addEventListener(`click`, function () {
   // Aggiungo un EventListener
+  if (button.innerText === `Accendi`){ // Con If verifico che il testo di button sia 'Accendi' per eseguire le sue modifiche:
   img.src = `./img/yellow_lamp.png`; // Punto a SRC dell'elemento img e lo cambio
   img.alt = `lampOn`; // Punto ad ALT dell'elemento img e lo cambio
-});
-
-button.addEventListener(`click`, function () {
-  if ((img.src = `./img/yellow_lamp.png`)) {
-    button.innerText = `Spegni`;
+  button.innerText = `Spegni`; // Punto al testo di button e lo cambio
   }
+ else if (button.innerText === `Spegni`)  { // Con Else If eseguo una verifica sempre sull testo di button, per eseguire le modifiche che mi riportano allo stato iniziale
+	  button.innerText = `Accendi`;
+      img.src = `./img/white_lamp.png`; 
+      img.alt = `lampOff`; 
+    };
 });
