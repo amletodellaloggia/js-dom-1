0fe94f8c2d46a06fb00fe5bc499eb45765fc0bd7
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-09 14:59:03 +0200

    [add] js (button event listener 2, temp function to change button text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const button = document.getElementById(`accendi`); // Dichiaro una variabile che accede all'id dell'elemento html button
 const img = document.getElementById(`lampOff`); // Dichiaro una variabile che accede all'id dell'elemento html img
 
-button.addEventListener(`click`, function(){ // Aggiungo un EventListener 
-img.src = `./img/yellow_lamp.png`; // Punto a SRC dell'elemento img e lo cambio 
-img.alt = `lampOn`; // Punto ad ALT dell'elemento img e lo cambio 
-})
+button.addEventListener(`click`, function () {
+  // Aggiungo un EventListener
+  img.src = `./img/yellow_lamp.png`; // Punto a SRC dell'elemento img e lo cambio
+  img.alt = `lampOn`; // Punto ad ALT dell'elemento img e lo cambio
+});
+
+button.addEventListener(`click`, function () {
+  if ((img.src = `./img/yellow_lamp.png`)) {
+    button.innerText = `Spegni`;
+  }
+});
