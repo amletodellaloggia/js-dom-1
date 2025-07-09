341e1a4cf0bff0288666c210454ff3e44ddd4dcb
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-09 14:33:05 +0200

    [add]js (variables, getelementbyid, eventlistener, changed img path to new img

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById(`accendi`); // Dichiaro una variabile che accede all'id dell'elemento html button
+const img = document.getElementById(`lampOff`); // Dichiaro una variabile che accede all'id dell'elemento html img
+
+button.addEventListener(`click`, function(){ // Aggiungo un EventListener 
+img.src = `./img/yellow_lamp.png`; // Punto a SRC dell'elemento img e lo cambio 
+img.alt = `lampOn`; // Punto ad ALT dell'elemento img e lo cambio 
+})
